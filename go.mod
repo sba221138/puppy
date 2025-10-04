1482751464fac51171b3fc242509984532e89a5d
sba221138 <thunder.alchemist21@gmail.com>
2025-10-04 23:07:16 +0800

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sba221138/puppy
 
 go 1.25.0
+
+require github.com/sba221138/dog v0.0.0-20251004080543-164c5900e5ef // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/sba221138/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
